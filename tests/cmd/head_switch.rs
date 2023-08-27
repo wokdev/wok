0ca3600dccf4088d99289c1aee0f5857ab76b6d2
Serge Matveenko <lig@countzero.co>
2023-08-27 15:54:04 +0200

    🚨 Make clippy check everything

--- tests/cmd/head_switch.rs
+++ tests/cmd/head_switch.rs
@@ -7,7 +7,7 @@ use wok::{self, cmd, config::Config};
 fn existing_branch(repo_sample: TestRepo, expected_config: String) {
     let mut actual_config = Config::load(&repo_sample.config_path()).unwrap();
 
-    _run(&format!("git switch other"), &repo_sample.repo_path).unwrap();
+    _run("git switch other", &repo_sample.repo_path).unwrap();
 
     cmd::head::switch(&mut actual_config, &repo_sample.repo()).unwrap();
 
